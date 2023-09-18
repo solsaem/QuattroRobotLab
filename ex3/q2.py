1fbd8ff8601729fb35087b5c3fa27291230f2f3d
JakeGreen85 <plc403@alumni.ku.dk>
2023-09-18 16:11:29 +0200

    Create q2.py

---
+++ ex3/q2.py
@@ -0,0 +1,14 @@
+from time import sleep
+import robot
+
+arlo = robot.Robot()
+
+
+def main():
+    while True:
+        # Rotate until find a landmark
+        # Drive 0.5 meters
+        # If front sensor < 300:
+            return
+
+main()
